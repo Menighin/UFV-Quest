ac9ee3ad2e4e2286cc64c6615bca8f512d16cab6
Menighin <joao.menighin@gmail.com>
2014-11-05 00:17:41 -0200

    Minor fix on calculate distance function

--- Backend/UFVQuest/UFVQuestAPI/ufvquest_utils.py
+++ Backend/UFVQuest/UFVQuestAPI/ufvquest_utils.py
@@ -3,7 +3,7 @@ from math import radians, cos, sin, asin, sqrt
 
 #Calculate distance in meters between the Lat and Lon passed and the Centro de Vivencia (-20.761717, -42.869958)
 def distance_to_centro_de_vivencia(lat, lon):
-	lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, -20.761717, -42.869958])
+	lon1, lat1, lon2, lat2 = map(radians, [float(lon), float(lat), -20.761717, -42.869958])
 	# haversine formula 
 	dlon = lon2 - lon1 
 	dlat = lat2 - lat1 
